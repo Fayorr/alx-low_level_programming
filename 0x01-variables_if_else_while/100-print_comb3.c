2780dedfb593c0064d294cbb84d4dab90514e573
Fayorr <fayokunmiosho@gmail.com>
2022-09-09 15:36:32 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 /**
-* main - entry point
-* Description - printing all possible
-* combinations of single digit numbers
+* main - print all numbers from 00 to 99 where the first numb is < last numb
 * Return: 0
 */
 int main(void)
 {
-	int g = 0;
+	int i, j;
 
-	while (g < 10)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(g + '0');
-		if (g < 9)
+		for (j = 0; j < 10; j++)
 		{
-			putchar(44);
-			putchar(32);
+			if (i < j)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				if (i * j != 72)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
 		}
-		g++;
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
