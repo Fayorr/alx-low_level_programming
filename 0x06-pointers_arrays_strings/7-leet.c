df931790e643b076d485c3b9cc90b0711e25d99c
Fayorr <fayokunmiosho@gmail.com>
2022-09-24 23:50:13 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* leet - encodes a string into 1337
+* @b: the string
+* Return: b
+*/
+char *leet(char *b)
+{
+	int i, j;
+	char *a = "aAeEoOtTlL";
+	char *A = "4433007711";
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (b[i] == a[j])
+			{
+				b[i] = A[j];
+			}
+		}
+	}
+	return (b);
+}
