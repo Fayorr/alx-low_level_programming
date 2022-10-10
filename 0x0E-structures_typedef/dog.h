96a9ceb97b016306a8b28b0ad3d857f7b1f998b9
Fayorr <fayokunmiosho@gmail.com>
2022-10-10 13:05:49 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,22 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG
+#define DOG
+int _putchar(char c);
 /**
-*struct dog - structure of a dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
+* struct dog - dog struct
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
 */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
-#endif /* STRTYP */
+void free_dog(dog_t *d);
+int _strlen(char *s);
+#endif
